bb96a1ea5b2e0ac7768f0c8c52801f2e82f63f8c
hongjinchai <hongjinchai@nxin.com>
2019-02-11 17:29:28 +0800

    ...

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 import {Form, Input, Icon, Button} from 'antd';
 import {randomNum} from '../../util/commonUtils';
 import './style.css';
 
+@Form.create()
 class LoginForm extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -18,33 +19,33 @@ class LoginForm extends React.Component {
     }
 
     generateVerificationCode = () => {
-        // const ctx = this.canvas.getContext('2d');
-        // const chars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-        // let code = '';
-        // ctx.clearRect(0, 0, 80, 39);
-        // for (let i = 0; i < 4; i++) {
-        //     const char = chars[randomNum(0, 57)];
-        //     code += char;
-        //     ctx.font = randomNum(20, 25) + 'px SimHei';     //设置字体随机大小
-        //     ctx.fillStyle = '#000000';
-        //     ctx.textBaseline = 'middle';
-        //     ctx.shadowOffsetX = randomNum(-3, 3);
-        //     ctx.shadowOffsetY = randomNum(-3, 3);
-        //     ctx.shadowBlur = randomNum(-3, 3);
-        //     ctx.shadowColor = 'rgba(0, 0, 0, 0.3)';
-        //     let x = 80 / 5 * (i + 1);
-        //     let y = 39 / 2;
-        //     let deg = randomNum(-25, 25);
-        //     /**设置旋转角度和坐标原点**/
-        //     ctx.translate(x, y);
-        //     ctx.rotate(deg * Math.PI / 180);
-        //     ctx.fillText(char, 0, 0);
-        //     /**恢复旋转角度和坐标原点**/
-        //     ctx.rotate(-deg * Math.PI / 180);
-        //     ctx.translate(-x, -y);
-        // }
+        const ctx = this.canvas.getContext('2d');
+        const chars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        let code = '';
+        ctx.clearRect(0, 0, 80, 39);
+        for (let i = 0; i < 4; i++) {
+            const char = chars[randomNum(0, 57)];
+            code += char;
+            ctx.font = randomNum(20, 25) + 'px SimHei';     //设置字体随机大小
+            ctx.fillStyle = '#000000';
+            ctx.textBaseline = 'middle';
+            ctx.shadowOffsetX = randomNum(-3, 3);
+            ctx.shadowOffsetY = randomNum(-3, 3);
+            ctx.shadowBlur = randomNum(-3, 3);
+            ctx.shadowColor = 'rgba(0, 0, 0, 0.3)';
+            let x = 80 / 5 * (i + 1);
+            let y = 39 / 2;
+            let deg = randomNum(-25, 25);
+            /**设置旋转角度和坐标原点**/
+            ctx.translate(x, y);
+            ctx.rotate(deg * Math.PI / 180);
+            ctx.fillText(char, 0, 0);
+            /**恢复旋转角度和坐标原点**/
+            ctx.rotate(-deg * Math.PI / 180);
+            ctx.translate(-x, -y);
+        }
         this.setState({
-            verificationCode: 'code'
+            verificationCode: code
         });
     };
 
@@ -84,8 +85,6 @@ class LoginForm extends React.Component {
     }
 }
 
-const MyLoginForm = Form.create()(LoginForm);
-
 class Login extends React.Component {
     constructor(props) {
         super(props);
@@ -100,7 +99,7 @@ class Login extends React.Component {
         return (
             <div id={'login-page'}>
                 <div className={'container'}>
-                    <MyLoginForm className={'box showBox'}/>
+                    <LoginForm className={'box showBox'}/>
                 </div>
             </div>
         );
