ab9dec16388478b041f1f706d8b7c416bdf562ae
hongjinchai <hongjinchai@nxin.com>
2019-02-14 12:33:28 +0800

    ...

--- src/components/Loadling/index.js
+++ src/components/Loadling/index.js
@@ -2,16 +2,32 @@ import React from 'react';
 import './style.css';
 
 class Loading extends React.Component {
-    render() {
+
+    renderLoading = (suffix) => {
         return (
-            <div id={'loading'}>
-                <div className={'dot'} />
-                <div className={'dot'} />
-                <div className={'dot'} />
-                <div className={'dot'} />
-                <div className={'dot'} />
+            <div>
+                <div className={'loading-' + suffix}>
+                    <span className={'dot'} />
+                    <span className={'dot'} />
+                    <span className={'dot'} />
+                    <span className={'dot'} />
+                    <span className={'dot'} />
+                </div>
+                <div className={'container-' + suffix}>
+                    {this.props.children}
+                </div>
             </div>
         );
+    };
+
+    render() {
+        const {loading} = this.props.loading;
+        if (loading) {
+            return this.renderLoading('show');
+        } else {
+            return this.renderLoading('hide');
+        }
     }
 }
+
 export default  Loading;
