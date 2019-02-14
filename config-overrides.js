6eeeaf3dc75b976d7ee8699a4021c7f080734229
hongjinchai <hongjinchai@nxin.com>
2019-02-14 13:59:09 +0800

    ...

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,14 @@
-const {override, addBabelPlugin, fixBabelImports} = require('customize-cra');
+const {override, addBabelPlugin, fixBabelImports, addLessLoader} = require('customize-cra');
 
 module.exports = override(
     // 启用ES7的修改器语法（babel 7）
     addBabelPlugin(['@babel/plugin-proposal-decorators', {'legacy': true}]),
 
     // antd按需加载
-    fixBabelImports('import', {libraryName: 'antd', libraryDirectory: 'es', style: true})
+    fixBabelImports('import', {libraryName: 'antd', libraryDirectory: 'es', style: true}),
+
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: { '@primary-color': '#4b4b4b' }
+    })
 );
