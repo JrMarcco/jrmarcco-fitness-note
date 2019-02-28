13333b5553660dbc6e70c4ffc722c2676a9bfe25
hongjinchai <hongjinchai@nxin.com>
2019-02-28 22:00:05 +0800

    commit message

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,27 +1,34 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 import {Popover, Avatar, Button, Icon} from 'antd';
+import {logout} from '../../util/authUtils';
 import './index.css';
 
-const title = (
-    <div id={'popover-title'}>
-        <Avatar size={64} icon="user"/>
-    </div>
-);
-const content = (
-    <div id={'popover-content'}>
-        <p>
-            <Button className={'btn'}>
-                <Icon type="logout"/>退出登录
-            </Button>
-        </p>
-    </div>
-);
-
+@withRouter
 class SidebarAvatar extends React.Component {
+    logout = () => {
+        logout();
+        this.props.history.push('/');
+    };
+
     render() {
         return (
             <div id={'avatar'}>
-                <Popover placement="rightBottom" title={title} content={content} trigger="click">
+                <Popover placement="rightBottom" title={(
+                    // Popover Title
+                    <div id={'popover-title'}>
+                        <Avatar size={64} icon="user"/>
+                    </div>
+                )} content={(
+                    // Popover Content
+                    <div id={'popover-content'}>
+                        <p>
+                            <Button className={'btn'} onClick={this.logout}>
+                                <Icon type="logout"/>退出登录
+                            </Button>
+                        </p>
+                    </div>
+                )} trigger="click">
                     <Avatar size={48} icon="user"/>
                 </Popover>
             </div>
