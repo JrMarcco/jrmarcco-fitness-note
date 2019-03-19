95ff7f02e3f7fbe9c77d14be036f1f35823b1285
hongjinchai <hongjinchai@nxin.com>
2019-03-19 10:38:28 +0800

    commit message

---
+++ src/util/loadableUtils.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Loadable from 'react-loadable';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+class LoadingPage extends React.Component {
+    componentDidMount() {
+        NProgress.start();
+    }
+
+    componentWillUnmount() {
+        NProgress.done();
+    }
+
+    render() {
+        return (
+            <div />
+        );
+    }
+}
+
+const LoadableComponent = (component) => {
+    return Loadable({
+        loader: component,
+        loading: () => <LoadingPage/>
+    });
+};
+
+export default LoadableComponent;
